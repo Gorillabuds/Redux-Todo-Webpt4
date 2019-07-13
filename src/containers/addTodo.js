9dcb0bf622241db21963c470cebef1f73c7822d0
JeffJohnson <jeff.jj.9494@gmail.com>
2019-07-13 12:43:56 -0600

    was in my root folder it would not let me add or commit from there whoops

--- src/containers/addTodo.js
+++ src/containers/addTodo.js
@@ -1,52 +1,71 @@
-import React, {Component} from "react";
-import {connect} from 'react-redux';
-import {addTodo,completeTodo} from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTodo, completeTodo, removeTodo } from "../actions";
 
-class AddTodo extends Component{
-    state={
-        currentItem:''
-    };
-    handleChange=event=>{
-        this.setState({currentItem:event.target.value});
-        console.log(this.state.currentItem);
-    };
-    submit =event=>{
-        event.preventDefault();
-        this.props.completeTodo(event.target.id);
-    };
-    toggleComplete=event=>{
-        event.preventDefault();
-        this.props.completeTodo(event.target.id);
-    };
-    render(){
-        return(
-            <div>
-                <form>
-                    <input
-                        onChange={this.handleChange}
-                        type="text"
-                        placeholder="Add Todo"
-                        value={this.state.currentItem}
-                    />
-                        <button onClick={this.submit}>Add Todo</button>
-                </form>
-                    <ul>
-                        {console.log(this.props.todo,'test')}
-                        {this.props.todo.map(item=>{
-                            return(
-                                <li
-                                    onClick={this.toggleComplete}
-                                    key={item.id}
-                                    id={item.id}
-                                    style={item.completed?
-                                    {textDecoration:"line-through"}:null
-                                    }>
-                                        {item.value}
-                                    </li>
-                            );
-                        })}
-                    </ul>
-            </div>
-        );
-    }
+ class AddTodo extends Component {
+  state = {
+    currentItem: ""
+  };
+
+   handleChange = event => {
+    this.setState({ currentItem: event.target.value });
+    console.log(this.state.currentItem);
+  };
+
+   submit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state.currentItem);
+    this.setState({ currentItem: "" });
+  };
+
+   toggleComplete = event => {
+    event.preventDefault();
+    this.props.completeTodo(event.target.id);
+  };
+  removegit = event =>{
+    event.preventDefault();
+    this.props.removeTodo(this.state.currentItem);
+    this.setState({currentItem:null})
+  }
+
+   render() {
+    return (
+      <div>
+        <form>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="New Todo Item"
+            value={this.state.currentItem}
+          />
+          <button onClick={this.submit}>Add to list</button>
+        </form>
+        <ul>
+          {console.log(this.props.todo, "check")}
+          {this.props.todo.map(item => {
+            return (
+              <li
+                onClick={this.toggleComplete}
+                key={item.id}
+                id={item.id}
+                style={
+                  item.completed ? { textDecoration: "line-through" } : null
+                }
+              >
+                {item.value} 
+              </li>
+            );
+          })}
+        </ul><button onClick={this.remove}>Remove from List</button>
+      </div>
+    );
+  }
 }
+
+ const mapStateToProps = state => {
+  return {
+    todo: state
+  };
+};
+
+ export default connect(mapStateToProps, { addTodo, completeTodo })(AddTodo);
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,23 +1,33 @@
-import {ADD_TODO, Toggle_Complete} from "../actions";
+import { ADD_TODO, TOGGLE_COMPLETE, REMOVE_TODO } from "../actions";
 
-export default (todo=[],action)=>{
-    console.log(action);
-    switch(action.type){
-        case ADD_TODO:
-            return[
-                ...todo,
-                {id:todo.length, value: action.payload, completed: false}
-            ];
-        case Toggle_Complete:
-        console.log('toggle',todo);
+ export default (todo = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...todo,
+        { id: todo.length, value: action.payload, completed: false }
+      ];
+    case TOGGLE_COMPLETE:
+      console.log("toggle", todo);
+      return todo.map(item => {
+        if (Number(item.id) === Number(action.id)) {
+          return { ...item, completed: !item.completed };
+        } else {
+          return item;
+        }
+      });
+      case REMOVE_TODO:
         return todo.map(item=>{
-            if(Number(item.id)===Number(action.id)){
-                return{ ...item,completed:!item.completed};
-            }else{
-                return item;
+          if(Number(item.id)===Number(action.id)){
+            return{
+              item
             }
-        });
-        default:
-            return todo;
-    }
-}
+          }else{
+            return item;
+          }
+        })
+     default:
+      return todo;
+  }
+};
